8d1a0b25889508a175bd0fa75ea067efdf814bc9
Nicholas Lamb <nickplamb@gmail.com>
2021-11-20 08:46:21 -0500

    generate welcome-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,14 +17,16 @@ import { FormsModule } from '@angular/forms';
 
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { BreedCardComponent } from './breed-card/breed-card.component'
+import { BreedCardComponent } from './breed-card/breed-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     UserLoginFormComponent,
-    BreedCardComponent
+    BreedCardComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
