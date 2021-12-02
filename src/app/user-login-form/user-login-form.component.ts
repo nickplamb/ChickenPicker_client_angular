f3ce36a99b35e94bab7fad8a9c82e0e1e7a063df
Nicholas Lamb <nickplamb@gmail.com>
2021-12-02 07:34:44 -0500

    reorganize code for readability, remove console.logs, set constructor arguments to private

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -5,6 +5,9 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { UserDataStoreService } from '../user-data-store.service';
+
+import { tUserData } from '../user-profile/user-profile.component';
 
 @Component({
   selector: 'app-user-login-form',
@@ -16,31 +19,36 @@ export class UserLoginFormComponent implements OnInit {
   @Input() userCredentials = { email: '', password: '' };
 
   constructor(
-    public fetchApiData: FetchApiDataService,
-    public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar,
-    public router: Router,
+    private fetchApiData: FetchApiDataService,
+    private userDataStore: UserDataStoreService,
+    private dialogRef: MatDialogRef<UserLoginFormComponent>,
+    private snackBar: MatSnackBar,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
   }
 
   loginUser(): void {
-    this.fetchApiData.userLogin(this.userCredentials).subscribe({ next: result => {
-      localStorage.setItem('token', result.token)
-      localStorage.setItem('userEmail', result.user.email);
-      localStorage.setItem('username', result.user.username);
-      console.log(result)
-      this.dialogRef.close();
-      this.router.navigate(['breeds']);
-      this.snackBar.open(result, 'OK', {
-        duration: 2000
-      });
-    }, error: result => {
-      this.snackBar.open(result, 'OK', {
-        duration: 2000
-      });
-    }});
+    this.fetchApiData.userLogin(this.userCredentials).subscribe({ 
+      next: response => {
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('userEmail', response.user.email);
+        localStorage.setItem('username', response.user.username);
+        localStorage.setItem('birthday', response.user.birthday);
+
+        this.dialogRef.close();
+        this.router.navigate(['breeds']);
+        console.log(response)
+        this.snackBar.open('Login Successful', 'OK', {
+          duration: 2000
+        });
+      }, error: response => {
+        this.snackBar.open('Something went wrong, please try again.', 'OK', {
+          duration: 2000
+        });
+      }
+    });
   }
 
 }
