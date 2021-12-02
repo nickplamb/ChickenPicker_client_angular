505ae2f971fe56ec698e418581cb4dd3987bc95f
Nicholas Lamb <nickplamb@gmail.com>
2021-12-02 07:32:27 -0500

    temp add stars and functions for add and remove from favorites. add post request fails.

--- src/app/breed-card/breed-card.component.ts
+++ src/app/breed-card/breed-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { BreedDescriptionService } from '../breed-description.service';
@@ -9,30 +10,52 @@ import { BreedDescriptionService } from '../breed-description.service';
   styleUrls: ['./breed-card.component.scss']
 })
 export class BreedCardComponent implements OnInit {
-  
+
   @Input() breedsToDisplay:any[] = [];
 
   breeds: any[] = [];
   backupImgUrl: string = '../../assets/breed_photos/frankie2.jpeg';
 
   constructor(
-    public fetchApiData: FetchApiDataService,
-    public convertBreedData: BreedDescriptionService
-    ) { }
+    private fetchApiData: FetchApiDataService,
+    public convertBreedData: BreedDescriptionService, // Public to be used in the template
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
-    // this.getBreeds();
   }
 
-  // getBreeds(): void {
-  //   this.fetchApiData.getAllBreeds().subscribe((response: any) => {
-  //     response.forEach((breed: any) => {
-  //       breed.imgUrl = `../../assets/breed_photos/${breed.breed.replace(/\s+/g, '').toLowerCase()}.jpg`;
-  //     });
-  //     this.breeds = response;
-  //     console.log(this.breeds[3])
-  //     // return this.breeds;
-  //   });
-  // }
+  addBreedToUserFavorites(breed: any): void {
+    console.log(breed)
+    this.fetchApiData.addToUserFavorites(breed._id).subscribe({
+      next: response => {
+        console.log(response);
+        this.snackBar.open(`${breed.breed} has been added to your favorites`, 'OK', {
+          duration: 2000
+        });
+      }, error: response => {
+        console.log(response);
+        this.snackBar.open(response, 'OK', {
+          duration: 2000
+        })
+      }
+    })
+  }
+
+  removeBreedFromUserFavorites(breed: any): void {
+    this.fetchApiData.removeFromUserFavorites(breed._id).subscribe({
+        next: response => {
+          console.log(response);
+          this.snackBar.open(`${breed.breed} has been removed from your favorites`, 'OK', {
+            duration: 2000
+          });
+        }, error: response => {
+          console.log(response);
+          this.snackBar.open(response, 'OK', {
+            duration: 2000
+          })
+        }
+      })
+  }
 
 }
