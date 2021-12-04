fb68f8a5392105062b4738c94cb56a5d96f9c5cb
Nicholas Lamb <nickplamb@gmail.com>
2021-12-04 13:25:14 -0500

    add subscription to userDataStore.userFavorites in component class, add conditional render in template of favorites icon to reflect if breed is already a favorite

--- src/app/breed-card/breed-card.component.ts
+++ src/app/breed-card/breed-card.component.ts
@@ -3,6 +3,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { BreedDescriptionService } from '../breed-description.service';
+import { UserDataStoreService } from '../user-data-store.service';
 
 @Component({
   selector: 'app-breed-card',
@@ -14,13 +15,21 @@ export class BreedCardComponent implements OnInit {
   @Input() breedsToDisplay:any[] = [];
 
   breeds: any[] = [];
+  userFavoriteBreeds: any[] = [];
   backupImgUrl: string = '../../assets/breed_photos/frankie2.jpeg';
+
+  _subscription_UserFavoriteBreeds;
 
   constructor(
     private fetchApiData: FetchApiDataService,
     public convertBreedData: BreedDescriptionService, // Public to be used in the template
+    public userDataStore: UserDataStoreService,
     private snackBar: MatSnackBar
-  ) { }
+  ) {
+    this._subscription_UserFavoriteBreeds = this.userDataStore.userFavorites.subscribe(data => {
+      this.userFavoriteBreeds = data.map((obj: any) => obj._id);
+    });
+  }
 
   ngOnInit(): void {
   }
@@ -45,7 +54,7 @@ export class BreedCardComponent implements OnInit {
   removeBreedFromUserFavorites(breed: any): void {
     this.fetchApiData.removeFromUserFavorites(breed._id).subscribe({
         next: response => {
-          console.log(response);
+          this.userDataStore.updateUserFavorites(response);
           this.snackBar.open(`${breed.breed} has been removed from your favorites`, 'OK', {
             duration: 2000
           });
@@ -59,3 +68,5 @@ export class BreedCardComponent implements OnInit {
   }
 
 }
+
+        // let favoritesArray = response.map((obj: any) => obj._id);
