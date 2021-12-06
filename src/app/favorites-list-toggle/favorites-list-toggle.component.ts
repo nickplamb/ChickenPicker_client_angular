cf568255eaec6ff53648be111b4a36531590c2c0
Nicholas Lamb <nickplamb@gmail.com>
2021-12-05 19:09:00 -0500

    move logic and template for favorites icon toggle to new component

--- src/app/favorites-list-toggle/favorites-list-toggle.component.ts
+++ src/app/favorites-list-toggle/favorites-list-toggle.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { UserDataStoreService } from '../user-data-store.service';
 
 @Component({
   selector: 'app-favorites-list-toggle',
@@ -7,9 +12,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesListToggleComponent implements OnInit {
 
-  constructor() { }
+  @Input() breed: Object;
+  @Input() isAFavorite: boolean;
+
+  constructor(
+    private fetchApiData: FetchApiDataService,
+    private userDataStore: UserDataStoreService,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  addBreedToUserFavorites(breed: any): void {
+    console.log(breed)
+    this.fetchApiData.addToUserFavorites(breed._id).subscribe({
+      next: response => {
+        console.log(response);
+        this.snackBar.open(`${breed.breed} has been added to your favorites`, 'OK', {
+          duration: 2000
+        });
+      }, error: response => {
+        console.log(response);
+        this.snackBar.open(response, 'OK', {
+          duration: 2000
+        })
+      }
+    })
+  }
+
+  removeBreedFromUserFavorites(breed: any): void {
+    this.fetchApiData.removeFromUserFavorites(breed._id).subscribe({
+        next: response => {
+          this.userDataStore.updateUserFavorites(response);
+          this.snackBar.open(`${breed.breed} has been removed from your favorites`, 'OK', {
+            duration: 2000
+          });
+        }, error: response => {
+          console.log(response);
+          this.snackBar.open(response, 'OK', {
+            duration: 2000
+          })
+        }
+      })
   }
 
 }
