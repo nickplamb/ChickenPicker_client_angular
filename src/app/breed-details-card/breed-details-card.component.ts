d49c0a93a460c36c637653d007650d39c367bd2f
Nicholas Lamb <nickplamb@gmail.com>
2021-12-05 19:15:50 -0500

    add template and Input for breed-details-card component

--- src/app/breed-details-card/breed-details-card.component.ts
+++ src/app/breed-details-card/breed-details-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { BreedDescriptionService } from '../breed-description.service';
 
 @Component({
   selector: 'app-breed-details-card',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreedDetailsCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() breed: any;
+
+  constructor(public convertBreedData: BreedDescriptionService) { }
 
   ngOnInit(): void {
   }
