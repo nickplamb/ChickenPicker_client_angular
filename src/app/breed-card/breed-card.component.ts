b0bebbfcb8f1794a9711152f4616dc3cf31cf5c1
Nicholas Lamb <nickplamb@gmail.com>
2021-12-07 17:01:34 -0500

    use favorites-list-toggle in breed-card, remove duplicate logic

--- src/app/breed-card/breed-card.component.ts
+++ src/app/breed-card/breed-card.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialog } from '@angular/material/dialog';
 
-import { FetchApiDataService } from '../fetch-api-data.service';
+/**
+ * Services
+ */
 import { BreedDescriptionService } from '../breed-description.service';
 import { UserDataStoreService } from '../user-data-store.service';
 
+/**
+ * Components
+ */
 import { BreedDetailsCardComponent } from '../breed-details-card/breed-details-card.component';
 import { ApaClassComponent } from '../apa-class/apa-class.component';
 import { BreedPurposeComponent } from '../breed-purpose/breed-purpose.component';
@@ -19,23 +23,13 @@ export class BreedCardComponent implements OnInit {
 
   @Input() breedsToDisplay:any[] = [];
 
-  breeds: any[] = [];
-  userFavoriteBreeds: any[] = [];
   backupImgUrl: string = '../../assets/breed_photos/frankie2.jpeg';
 
-  _subscription_UserFavoriteBreeds;
-
   constructor(
-    private fetchApiData: FetchApiDataService,
     public convertBreedData: BreedDescriptionService, // Public to be used in the template
     public userDataStore: UserDataStoreService,
-    private snackBar: MatSnackBar,
     private dialog: MatDialog,
-  ) {
-    this._subscription_UserFavoriteBreeds = this.userDataStore.userFavorites.subscribe(data => {
-      this.userFavoriteBreeds = data.map((obj: any) => obj._id);
-    });
-  }
+  ) { }
 
   ngOnInit(): void {
   }
@@ -46,9 +40,9 @@ export class BreedCardComponent implements OnInit {
    * 
    * Opens the dialog displaying more details about a single breed
    */
-  openBreedDetailsDialog(breed: any): void {
+  public openBreedDetailsDialog(breed: any): void {
     this.dialog.open(BreedDetailsCardComponent, {
-      width: '400px',
+      width: '450px',
       data: {
         breed: breed,
       }
@@ -61,9 +55,9 @@ export class BreedCardComponent implements OnInit {
    * 
    * Opens the dialog displaying details about the APA Class
    */
-  openApaClassDetailsDialog(apaClass: string): void {
+  public openApaClassDetailsDialog(apaClass: string): void {
     this.dialog.open(ApaClassComponent, {
-      width: '400px',
+      width: '600px',
       data: {
         apaClass: apaClass
       }
@@ -76,46 +70,12 @@ export class BreedCardComponent implements OnInit {
    *  
    * Opens the dialog displaying details the breeds purpose
    */
-  openBreedPurposeDetailsDialog(purpose: string): void {
+  public openBreedPurposeDetailsDialog(purpose: string): void {
     this.dialog.open(BreedPurposeComponent, {
-      width: '400px',
+      width: '600px',
       data: {
         breedPurpose: purpose
       },
     });
   }
-
-  addBreedToUserFavorites(breed: any): void {
-    console.log(breed)
-    this.fetchApiData.addToUserFavorites(breed._id).subscribe({
-      next: response => {
-        console.log(response);
-        this.snackBar.open(`${breed.breed} has been added to your favorites`, 'OK', {
-          duration: 2000
-        });
-      }, error: response => {
-        console.log(response);
-        this.snackBar.open(response, 'OK', {
-          duration: 2000
-        })
-      }
-    })
-  }
-
-  removeBreedFromUserFavorites(breed: any): void {
-    this.fetchApiData.removeFromUserFavorites(breed._id).subscribe({
-        next: response => {
-          this.userDataStore.updateUserFavorites(response);
-          this.snackBar.open(`${breed.breed} has been removed from your favorites`, 'OK', {
-            duration: 2000
-          });
-        }, error: response => {
-          console.log(response);
-          this.snackBar.open(response, 'OK', {
-            duration: 2000
-          })
-        }
-      })
-  }
-
 }
--- src/app/favorites-list-toggle/favorites-list-toggle.component.ts
+++ src/app/favorites-list-toggle/favorites-list-toggle.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+/**
+ * Services
+ */
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { UserDataStoreService } from '../user-data-store.service';
 
@@ -12,24 +14,38 @@ import { UserDataStoreService } from '../user-data-store.service';
 })
 export class FavoritesListToggleComponent implements OnInit {
 
-  @Input() breed: Object;
-  @Input() isAFavorite: boolean;
+  @Input() breedId: string;
+  @Input() breedName: string;
+
+  userFavoriteBreedsIdsArray: any[] = [];
+
+  _subscription_UserFavoriteBreeds;
 
   constructor(
     private fetchApiData: FetchApiDataService,
     private userDataStore: UserDataStoreService,
     private snackBar: MatSnackBar
-  ) { }
+  ) {
+    this._subscription_UserFavoriteBreeds = this.userDataStore.userFavorites.subscribe(data => {
+      this.userFavoriteBreedsIdsArray = data.map((obj: any) => obj._id);
+    });
+  }
 
   ngOnInit(): void {
   }
 
-  addBreedToUserFavorites(breed: any): void {
-    console.log(breed)
-    this.fetchApiData.addToUserFavorites(breed._id).subscribe({
+  /**
+   * 
+   * @param breedId 
+   * @param breedName 
+   * 
+   * Sends Post request to API with breed ID as URL parameter to add the breed to the users favorites list.
+   */
+  addBreedToUserFavorites(breedId: string, breedName: string): void {
+    this.fetchApiData.addToUserFavorites(breedId).subscribe({
       next: response => {
         this.userDataStore.updateUserFavorites(response);
-        this.snackBar.open(`${breed.breed} has been added to your favorites`, 'OK', {
+        this.snackBar.open(`${breedName} has been added to your favorites`, 'OK', {
           duration: 2000
         });
       }, error: response => {
@@ -38,23 +54,29 @@ export class FavoritesListToggleComponent implements OnInit {
           duration: 2000
         })
       }
-    })
+    });
   }
 
-  removeBreedFromUserFavorites(breed: any): void {
-    this.fetchApiData.removeFromUserFavorites(breed._id).subscribe({
-        next: response => {
-          this.userDataStore.updateUserFavorites(response);
-          this.snackBar.open(`${breed.breed} has been removed from your favorites`, 'OK', {
-            duration: 2000
-          });
-        }, error: response => {
-          console.log(response);
-          this.snackBar.open(response, 'OK', {
-            duration: 2000
-          })
-        }
-      })
+  /**
+   * 
+   * @param breedId 
+   * @param breedName 
+   * 
+   * Send Delete request to API with breed ID as URL parameter to remove the breed from the users favorites list.
+   */
+  removeBreedFromUserFavorites(breedId: string, breedName: string): void {
+    this.fetchApiData.removeFromUserFavorites(breedId).subscribe({
+      next: response => {
+        this.userDataStore.updateUserFavorites(response);
+        this.snackBar.open(`${breedName} has been removed from your favorites`, 'OK', {
+          duration: 2000
+        });
+      }, error: response => {
+        console.log(response);
+        this.snackBar.open(response, 'OK', {
+          duration: 2000
+        })
+      }
+    });
   }
-
 }
